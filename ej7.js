ff36aa874df66e740ad68823479604deda472b87
TOBIAS ROHR <49549066@ort>
2026-03-20 11:40:31 -0300

    gemini

---
+++ ej7.js
@@ -0,0 +1,16 @@
+import { getCurrency } from 'currency-map-country '
+
+let monedaDelPais, codigoPais;
+codigoPais = 'AR';
+monedaDelPais = obtenerMoneda(codigoPais);
+console.log(`La moneda del país ${codigoPais} es: ${monedaDelPais}`);
+codigoPais = 'UZA';
+monedaDelPais = obtenerMoneda(codigoPais);
+console.log(`La moneda del país ${codigoPais} es: ${monedaDelPais}`);
+
+console.log(getCurrency(codigoPais))
+
+
+//function obtenerMoneda(codigoPais)
+
+//return getCurrency(codigoPais) ; // => { name: 'Dólar estadounidense (USD)', symbolFormat: '${#}' } 
