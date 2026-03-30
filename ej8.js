ab1dc7138c54a413fd0ca0308ec01b98999239e9
Android Studio <devandroid@ort>
2026-03-30 09:04:01 -0300

    falta terminar el 3, lo estamos haciendo

--- ej8.js
+++ ej8.js
@@ -1,7 +1,15 @@
-import {OMDBSearchByPage, OMDBSearchComplete,OMDBGetByImdbID} from './modules/omdb-wrapper.js'
+import {OMDBSearchByPage, OMDBSearchComplete,OMDBGetByImdbID} from './src/modules/omdb-wrapped.js'
 let resultado = null;
 // Test de la función OMDBSearchByPage..
-resultado = await OMDBSearchByPage("Back to the Future", 1);
-console.log("OMDBSearchByPage", resultado);
 
+//EJERCICIO 1
+//resultado = await OMDBSearchByPage("Back to the Future", 1);
+//console.log("OMDBSearchByPage", resultado);
 
+//EJERCICIO 2 
+//resultado= await OMDBSearchComplete("Back to the Future")
+//console.log("OMDBSearchComplete", resultado)
+
+//EJERICIO 3 
+resultado = await OMDBGetByImdbID("tt0088763")
+console.log("OMDBGetByImdbID", resultado)
---
+++ src/modules/omdb-wrapped.js
@@ -0,0 +1,80 @@
+/* Módulo OMDBWrapper*/
+import axios from "axios";
+const APIKEY = "4ca2c568"; // Poné tu APIKEY, esta no funciona.
+const OMDBSearchByPage = async (searchText, page = 1) => {
+let returnObject = {
+respuesta : false,
+cantidadTotal : 0,
+datos : []
+};
+
+let url  = 'http://www.omdbapi.com/?' + 'apikey=' + APIKEY + '&' + 'page=1'  + '&' + 's=back+to+the+future'
+
+let response = await axios.get(url)
+
+if(response.data.Response === "True"){
+    returnObject.respuesta = true;
+    returnObject.cantidadTotal = response.data.totalResults;
+    returnObject.datos = response.data.Search;
+}
+
+// No seas vago, acá hay que hacer el cuerpo de la función!!!
+return returnObject;
+};
+const OMDBSearchComplete = async (searchText) => {
+let returnObject = {
+respuesta : false,
+cantidadTotal : 0,
+datos : []
+};
+let pagina= 1
+for (let i = 0; i <= 8; i++)
+{
+let url  = 'http://www.omdbapi.com/?' + 'apikey=' + APIKEY + '&s=' + searchText  + '&page=' + pagina
+let response = await axios.get(url)
+
+if(response.data.Response === "True"){
+    returnObject.respuesta = true;
+    returnObject.cantidadTotal = response.data.totalResults;
+ returnObject.datos = returnObject.datos.concat(response.data.Search);
+}
+
+
+pagina++
+}
+
+
+return returnObject;
+
+};
+
+
+
+
+
+
+const OMDBGetByImdbID = async (imdbID) => {
+let returnObject = {
+respuesta : false,
+cantidadTotal : 0,
+datos : {}
+};
+
+let url  = 'http://www.omdbapi.com/?' + 'apikey=' + APIKEY + '&' + 'page=1'  + '&' + 'i=' + imdbID
+
+let response = await axios.get(url)
+
+if(response.data.Response === "True"){
+    returnObject.respuesta = true;
+    returnObject.cantidadTotal = response.data.totalResults;
+    returnObject.datos = response.data.Search;
+}
+
+
+return returnObject;
+};
+
+
+
+// Exporto todo lo que yo quiero exponer del módulo:
+export {OMDBSearchByPage, OMDBSearchComplete, OMDBGetByImdbID};
